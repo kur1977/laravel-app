88703d9bfeb97663451d7d1d8b7ddca094457fa0
kur1977 <kur1977@gmail.com>
2021-09-23 19:56:05 +0300

    разбивка темплейтов

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'body', 'img', 'slug'];
+
+    public $dates = ['published_at'];
 
     //protected $guarded = [];
 
@@ -23,5 +26,21 @@ class Article extends Model
 
     public function tags(){
         return $this->belongsToMany(Tag::class);
+    }
+
+    public function getBodyPreview(){
+        return Str::limit($this->body, 100);
+    }
+
+    public function createdAtForHumans(){
+        return $this->created_at->diffForHumans();
+    }
+
+    public function publishedAtForHumans(){
+        return $this->published_at->diffForHumans();
+    }
+
+    public function scopeLastLimit($query, $limit){
+        return $query->with('state', 'tags')->orderBy('created_at', 'desc')->limit($limit)->get();
     }
 }
